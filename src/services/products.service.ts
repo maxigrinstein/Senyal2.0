6653cfa32f7730e035b4f0f18645580b1b1c0edb
maxigrinstein <maxigrinstein@gmail.com>
2024-03-14 03:13:13 -0300

    Colors fixes, added footer

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,50 +1,53 @@
+
+import { v4 as uuidv4 } from 'uuid';
+
 const products = [
 	{
-		id: "1",
+		id: uuidv4(),
 		title: "Camisa de algodon",
 		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
 		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
 	},
 	{
-		id: "2",
-		title: "Camisa de algodon",
-		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
-		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
-
-	},
-	{
-		id: "3",
-		title: "Camisa de algodon",
-		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
-		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
-	},
-	{
-		id: "4",
-		title: "Camisa de algodon",
-		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
-		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
-	},
-	{
-		id: "5",
-		title: "Camisa de algodon",
-		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
-		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
-	},
-	{
-		id: "6",
+		id: uuidv4(),
 		title: "Camisa de algodon",
 		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
 		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
 
 	},
 	{
-		id: "7",
+		id: uuidv4(),
 		title: "Camisa de algodon",
 		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
 		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
 	},
 	{
-		id: "8",
+		id: uuidv4(),
+		title: "Camisa de algodon",
+		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
+		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
+	},
+	{
+		id: uuidv4(),
+		title: "Camisa de algodon",
+		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
+		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
+	},
+	{
+		id: uuidv4(),
+		title: "Camisa de algodon",
+		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
+		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
+
+	},
+	{
+		id: uuidv4(),
+		title: "Camisa de algodon",
+		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
+		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
+	},
+	{
+		id: uuidv4(),
 		title: "Camisa de algodon",
 		description: "Camisa de manga larga con botones, disponible en varios colores y tallas",
 		image: "/378542495_17995575353499155_6681925949692402668_n.jpg"
