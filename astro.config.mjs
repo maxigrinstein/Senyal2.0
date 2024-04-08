28ac03b081426276308df6c8edd4dc4471dd6fae
maxigrinstein <maxigrinstein@gmail.com>
2024-04-08 00:43:41 -0300

    deploy update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   integrations: [tailwind(), icon()],
   output: "server",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
 });
